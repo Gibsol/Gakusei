1f26953d11c4ca082a22762145e8f607114fc820
Gibsol <gibsol75@gmail.com>
2022-06-22 11:51:23 +0300

    added an example of usage and other stuff

---
+++ src/example.cpp
@@ -0,0 +1,20 @@
+// A brief example of working with this library
+
+#include <vector>
+#include "student_manager.cpp"
+
+int main() {
+    std::vector<StudentManager> grads;
+    auto* st1 = new StudentManager("john", 2.0, 2020, 45);
+
+    if(st1->get_gpa() >=3.0) { grads.push_back(*st1); }
+    st1->calc_grade(st1->get_gpa());
+
+    grads.push_back(*st1);
+
+    std::cout << st1->get_grade();
+
+    delete st1;
+
+    return 0;
+}
--- src/student_manager.h
+++ src/student_manager.h
@@ -6,17 +6,28 @@
 class StudentManager {
 private:
     std::string name;
-    int year;
-    int gpa;
+    char grade;
+    double gpa;
+    int year; // The year of admission
     int age;
 public:
     StudentManager();
-    StudentManager(std::string, int, int, int);
+    StudentManager(std::string, double, int, int);
+
+    std::string get_name();
+    char get_grade();
+    double get_gpa();
+    int get_year();
+    int get_age();
 
     void set_name(std::string);
+    void set_grade(char);
+    void set_gpa(double);
     void set_year(int);
-    void set_gpa(int);
     void set_age(int);
+
+    char calc_grade(double);
+    void show_info();
 };
 
 
