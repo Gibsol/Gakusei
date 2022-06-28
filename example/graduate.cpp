be4e4275254de3b1edb74d46dbcfc15b6567aa6b
Gibsol <gibsol75@gmail.com>
2022-06-28 11:32:10 +0300

    example fix

--- example/graduate.cpp
+++ example/graduate.cpp
@@ -1,18 +1,18 @@
 // A brief example of working with this library
 
 #include <vector>
-#include "../src/student.cpp"
 
-using namespace Gakusei;
+#include "../gakusei/src/student.cpp"
 
 int main() {
-    std::vector<Student> grads;
-    auto* st1 = new Student("john", 2.0, 2020, 45);
+
+    std::vector<Gakusei::Student> grads;
+    auto* st1 = new Gakusei::Student("john", 2.0, 2020, 45);
 
     if(st1->get_gpa() >=3.0) { grads.push_back(*st1); }
     st1->calc_grade(st1->get_gpa());
 
-    std::cout << st1->get_grade();
+    std::cout << st1->get_grade() << std::endl;
 
     delete st1;
 
